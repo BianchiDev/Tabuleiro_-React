ef44be0252698ecf86fe4de9175d5e37004981d2
BianchiDev <ifpebianchi@gmail.com>
2022-10-09 16:11:53 -0300

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import Linha from '../components/Linha'
+import Subdivisao from '../components/Subdivisao'
+import Tabuleiro from '../components/Tabuleiro'
+import styles from '../styles/Home.module.css'
+
+
+export default function Home() {
+  return (
+    <>
+     <div className={styles.container}>
+     <Tabuleiro/>
+    </div>
+    </>
+  )
+}
